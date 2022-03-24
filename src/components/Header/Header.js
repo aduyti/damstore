c52a1f98557df059c5bdd5ecd1b5311ce1abc811
Anirudhya <anirudhyaduyti@gmail.com>
2022-03-24 21:58:53 +0600

    header content

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './Header.css';
+import Logo from '../../images/Logo.svg';
 
 const Header = () => {
     return (
-        <div>
-            head
+        <div className="header">
+            <div><a href="#home"><img src={Logo} alt="" /></a></div>
+            <div>
+                <a href="#order">Order</a>
+                <a href="#order-review">Order Review</a>
+                <a href="#inventory">Manage Inventory</a>
+            </div>
         </div>
     );
 };
