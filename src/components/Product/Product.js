5e3a0ec24ec42e5d0054c1b17c28ad41a9c2add6
Anirudhya <anirudhyaduyti@gmail.com>
2022-03-24 23:59:45 +0600

    card info

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -9,7 +9,13 @@ const Product = ({ product, clickHandler }) => {
             <div className="card-info">
                 <h3 style={{ fontSize: '21px' }} className="card-text text-black">{name}</h3>
                 <h5 style={{ fontSize: '17px' }} className="card-text text-black">Price: ${price}</h5>
+                <p className="card-text text-gray">
+                    Manufacturer: {seller}
+                    <br />
+                    Rating: {ratings}
+                </p>
             </div>
+            <button className="card-btn" onClick={() => clickHandler(id)}>Add to Cart</button>
         </div>
     );
 };
