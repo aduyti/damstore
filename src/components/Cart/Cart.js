8054f941ef6658df31f16c47bd789b73ba317e8c
Anirudhya <anirudhyaduyti@gmail.com>
2022-03-24 23:06:31 +0600

    shop layout

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = () => {
     return (
-        <div>
+        <div className="cart">
 
         </div>
     );
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import Products from '../Products/Products.js';
-import Cart from '../Cart/Cart.js'
+import React, { useEffect, useState } from 'react';
+import { addToDb } from '../../fakeData/fakeDB.js';
+import Product from '../Product/Product.js';
+import Cart from '../Cart/Cart.js';
+import './Shop.css'
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, [])
+    const handelClick = id => addToDb(id);
+
     return (
-        <div>
-            <Products></Products>
+        <div className="shop">
+            <div>
+                {
+                    products.map(product => <Product product={product} key={product.id} clickHandler={handelClick} />)
+                }
+            </div>
             <Cart />
-            shop
         </div>
     );
 };
