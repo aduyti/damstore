4deb0ad0a70e639ead25e35f20a3256a873900eb
Anirudhya <anirudhyaduyti@gmail.com>
2022-03-25 00:40:10 +0600

    card btn position

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -9,10 +9,13 @@ const Product = ({ product, clickHandler }) => {
             <div className="card-info">
                 <h3 style={{ fontSize: '21px' }} className="card-text text-black">{name}</h3>
                 <h5 style={{ fontSize: '17px' }} className="card-text text-black">Price: ${price}</h5>
-                <p style={{ paddingTop: '36px' }} className="card-text text-gray">Manufacturer: {seller}</p>
-                <p className="card-text text-gray">Rating: {ratings}</p>
             </div>
-            <button className="card-btn" onClick={() => clickHandler(id)}>Add to Cart</button>
+            <div className="card-bottom">
+
+                <p className="card-info card-text text-gray">Manufacturer: {seller}</p>
+                <p className="card-info card-text text-gray">Rating: {ratings}</p>
+                <button className="card-btn" onClick={() => clickHandler(id)}>Add to Cart</button>
+            </div>
         </div>
     );
 };
