e2a2d865f77ac6a0c3e90afe47e08b791698aaaf
Anirudhya <anirudhyaduyti@gmail.com>
2022-03-27 20:25:17 +0600

    click store

--- src/fakeData/fakeDB.js
+++ src/fakeData/fakeDB.js
@@ -1,6 +1,12 @@
 const addToDb = id => {
-    console.log(id);
-    // localStorage.getItem('shopping-cart');
+    const cart = JSON.parse(localStorage.getItem('shopping-cart')) || {};
+    if (cart[id]) {
+        cart[id] += 1;
+    }
+    else {
+        cart[id] = 1;
+    }
+    localStorage.setItem('shopping-cart', JSON.stringify(cart));
 }
 const getDbData = () => {
     localStorage.getItem('shopping-cart');
